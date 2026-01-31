d832b834511caddbd900709e0cfb3f283f96aaf5
DianaDmK <kalyakina.130802@mail.ru>
2026-02-01 03:17:54 +0700

    feat: 4.16. Divide and conquer

--- src/mocks/points-mock.js
+++ src/mocks/points-mock.js
@@ -1,31 +1,32 @@
-import { getRandomIntInRange, getRandomArrayItem, getRandomDate, addRandomDuration, getRandomSubarray } from '../utils.js';
+import { getRandomIntInRange, getRandomArrayItem, getRandomDate, addRandomDuration } from '../utils.js';
 import {
   TYPES,
   MIN_BASE_PRICE,
   MAX_BASE_PRICE,
   MAX_DURATION_HOURS,
-  POINT_COUNT
+  POINT_COUNT,
+  CITIES
 } from '../const.js';
 import { DESTINATIONS } from './destinations-mock.js';
-import { OFFERS } from './offers-mock.js';
 
 function generateRoutePoint() {
+  const cityName = getRandomArrayItem(CITIES);
   const type = getRandomArrayItem(TYPES);
   const destination = getRandomArrayItem(DESTINATIONS);
   const startTime = getRandomDate();
   const endTime = addRandomDuration(startTime, MAX_DURATION_HOURS);
   const basePrice = getRandomIntInRange(MIN_BASE_PRICE, MAX_BASE_PRICE);
-  const availableOffers = OFFERS[type] || [];
-  const selectedOffers = getRandomSubarray(availableOffers).map((o) => o.id);
+  const availableOffers = [];
 
   return {
     id: crypto.randomUUID(),
+    cityName,
     type,
     destinationId: destination.id,
     startTime,
     endTime,
     basePrice,
-    offers: selectedOffers
+    offers: availableOffers
   };
 }
 
--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -4,35 +4,65 @@ import FiltersView from '../view/filters-view.js';
 import RoutePointView from '../view/route-point-view.js';
 import SortView from '../view/sort-view.js';
 import PointListView from '../view/point-list-view.js';
-import { render, RenderPosition } from '../render.js';
+import { render, RenderPosition, replace } from '../framework/render.js';
 
 export default class MainPresenter {
-  constructor(tripModel) {
-    this.model = tripModel;
+  #tripModel = null;
+  #pointListView = new PointListView();
 
+  constructor(tripModel) {
     this.tripMainContainer = document.querySelector('.trip-main');
     this.filtersContainer = document.querySelector('.trip-controls__filters');
     this.tripEventsContainer = document.querySelector('.trip-events');
+
+    this.#tripModel = tripModel;
   }
 
   init() {
-    const pointListView = new PointListView();
-    const { points, destination, offers } = this.model;
+    const { points, destination, offers } = this.#tripModel;
 
     render(new TripInfoView(), this.tripMainContainer, RenderPosition.AFTERBEGIN);
     render(new FiltersView(), this.filtersContainer);
     render(new SortView(), this.tripEventsContainer, RenderPosition.AFTERBEGIN);
-    render(pointListView, this.tripEventsContainer);
+    render(this.#pointListView, this.tripEventsContainer);
 
-    if (points.length > 0) {
-      const editFormView = new EditFormView({ point: points[0], destination, offers });
-      render(editFormView, pointListView.getElement());
-    }
-    const safeDestination = Array.isArray(destination) ? destination : [];
+    const pointListViewContainer = this.#pointListView.element;
 
     points.forEach((point) => {
-      const pointView = new RoutePointView(point, safeDestination);
-      render(pointView, pointListView.getElement());
+      this.#renderPointView(point, destination, offers, pointListViewContainer);
     });
+  }
+
+  #renderPointView(point, destination, offers) {
+    const escKeyDownHandler = (evt) => {
+      if (evt.key === 'Escape') {
+        evt.preventDefault();
+        replaceFormToCard();
+        document.removeEventListener('keydown', escKeyDownHandler);
+      }
+    };
+    const pointComponent = new RoutePointView({
+      point, destination, offers,
+      onEditClick: () => {
+        replaceCardToForm();
+        document.addEventListener('keydown', escKeyDownHandler);
+      }
+    });
+    const pointEditComponent = new EditFormView({
+      point, destination, offers,
+      onFormSubmit: () => {
+        replaceFormToCard();
+        document.removeEventListener('keydown', escKeyDownHandler);
+      }
+    });
+
+    function replaceCardToForm() {
+      replace(pointEditComponent, pointComponent);
+    }
+
+    function replaceFormToCard() {
+      replace(pointComponent, pointEditComponent);
+    }
+    render(pointComponent, this.#pointListView.element);
   }
 }
